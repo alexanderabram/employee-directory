4ce1c74b2100bdd2927d6beb0e00702e0f87fc8c
AMA <alexanderabram6440@gmail.com>
2020-07-21 17:57:31 -0500

    added main function to main.js

--- public/main.js
+++ public/main.js
@@ -2,3 +2,12 @@ import React from "react";
 import DataArea from "./DataArea";
 import "../styles/Main.css";
 
+function Main() {
+    return (
+        <>
+            <DataArea />
+        </>
+    );
+};
+
+export default Main;
