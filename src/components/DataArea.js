b80ebf82a53a46638db6d004d49798e7ba34923e
AMA <alexanderabram6440@gmail.com>
2020-07-21 17:44:04 -0500

    added DataArea const

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -4,3 +4,17 @@ import Nav from "./Nav";
 import API from "../utils/API";
 import "../styles/DataArea.css";
 import DataAreaContext from "../utils/DataAreaContext";
+
+const DataArea = () => {
+    const [developerState, setDeveloperState] = useState({
+        users: [],
+        order: "descend",
+        filteredUsers: [],
+        headings: [
+            { name: "Image", width: "10%", order: "descend" },
+            { name: "name", width: "10%", order: "descend" },
+            { name: "phone", width: "20%", order: "descend" },
+            { name: "email", width: "20%", order: "descend" },
+            { name: "dob", width: "10%", order: "descend" }
+        ]
+    });
