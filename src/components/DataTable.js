1e7ed2b88d4618ffb5aeda7d05431c893c5b266d
AMA <alexanderabram6440@gmail.com>
2020-07-21 17:53:20 -0500

    creating DataTable function in DataTable

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,3 +2,6 @@ import React, { useContext } from "react";
 import DataBody from "./DataBody";
 import "../styles/DataTable.css";
 import DataAreaContext from "../utils/DataAreaContext";
+
+const DataTable = () => {
+    const context = useContext(DataAreaContext);
