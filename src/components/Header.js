d1da0be529bf553095e591ec38e79b54c5897414
AMA <alexanderabram6440@gmail.com>
2020-07-21 19:23:54 -0500

    added header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="header">
             <h1>Employee Directory</h1>
-            <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+            <p>Search for employee by name or use the arrows to sort</p>
         </div>
     )
 }
